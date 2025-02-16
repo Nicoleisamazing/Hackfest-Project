f5c7dca51265834117b632bc9f341fb66c0b14a4
Nicoleisamazing <zhangnicole2022@gmail.com>
2025-02-16 13:23:35 -0800

    python version

---
+++ question and answer.py
@@ -0,0 +1,7 @@
+emergencies = input("How many people need immediate first aid? ")
+supplies = input("What supplies do you need? ")
+transport = input("What would be the best way to transport your supplies? ")
+location = input("Where should the supplies be dropped off? ")
+population = input("How many people are in need? ")
+
+print(supplies, transport, location)
