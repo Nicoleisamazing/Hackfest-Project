1b130365c88affbc5e1a2080d0f40b94b8734ab6
Nicoleisamazing <zhangnicole2022@gmail.com>
2025-02-16 13:30:08 -0800

    m

--- question and answer.py
+++ question and answer.py
@@ -3,5 +3,3 @@ supplies = input("What supplies do you need? ")
 transport = input("What would be the best way to transport your supplies? ")
 location = input("Where should the supplies be dropped off? ")
 population = input("How many people are in need? ")
-
-print(supplies, transport, location)
